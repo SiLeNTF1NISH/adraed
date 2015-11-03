66256e51512b286c771dd5f7b85217ae9482bd39
SiLeNTF1NISH <dannyv1997@hotmail.com>
2015-11-03 01:03:05 -0500

    initial commit

---
+++ Resources/Modules/header.php
@@ -0,0 +1,8 @@
+<html>
+<header>
+<title>Silent's Nation: Adraed</title>
+<meta charset="utf-8">
+<meta name="description" content="This website was made for the nation Adraed as a way to connect all of those
+								  who are in the nation or other allied nations and towns.">
+<meta name="keywords" content="Adraed,EvolveEconomy,Evolve,Avellana,Silent_Finish,Silent_F1nish,TurtleStomper,Preloom,Adraed Forum">
+<meta name="author" content="TurtleStomper">
---
+++ index.php
@@ -0,0 +1,38 @@
+<!----We do not need a header here as it's included in the PHP file MAKE SURE to add </header> for anything you need to add page specific----->
+<?php
+include 'resources/modules/header.php';
+?>
+<link rel="stylesheet" type="text/css" href="styles/homepagestyles.css">
+</header>
+<body style="background-color:purple;">
+
+<div id="BannerPhoto"><img src="#" alt="Banner Photo"></img></div>
+<div id="navbarwrapper">
+<div id="navbar">
+<ul>
+<li><a href="#">| Home |</a></li>
+<li><a href="#">| Forum |</a></li>
+<li><a href="#">| Members |</a></li>
+<li><a href="#">| Contact |</a>
+<div id="dropdownwrapper">
+<ul>
+<li><a href="contactdev.php">Website Dev</a></li>
+<li><a href="contactsilent.php">Silent_Finish</a></li>
+<li><a href="generalticket.php">General Inquiry</a></li>
+</ul>
+</div>
+</li>
+</ul>
+</div>
+</div>
+<div id="loginbox">
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
+
+</div> <!---INSERT LOGIN SCRIPT HERE, For now it is just a box.---->
+<div id="rightsidebarwrapper">
+</div>
+<div id="Maincontentwrapper">
+</div>
+</body>
+</html>
